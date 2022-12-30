2c7d8b0271419833588fb7154fac8680aa792bbe
Vitalii-Bilobrov <106316488+Vitalii-Bilobrov@users.noreply.github.com>
2022-12-30 12:30:50 +0200

    Final 30_12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,30 +4,16 @@ import { Section } from './Section';
 import { Notification } from './Notification';
 import { useState } from 'react';
 
-// export class App extends Component {
-//   static defaultProps = {
-//     initialValue: 1,
-//   };
-
-//   static propTypes = {};
-
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
 export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const countTotalFeedback = () => {
-    // const { good, neutral, bad } = this.state;
     return good + neutral + bad;
   };
 
   const countPositiveFeedbackPercentage = () => {
-    // const { good, neutral, bad } = this.state;
     return Math.round((good / (good + neutral + bad)) * 100);
   };
 
@@ -54,7 +40,6 @@ export const App = () => {
     }
   };
 
-  // const { good, neutral, bad } = this.state;
   return (
     <>
       <Section title="Please leave feedback">
